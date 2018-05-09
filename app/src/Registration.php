209cd6ce9028c082babfd6d1b47e0f77bbdc63d6
Al Twohill <al@twohill.nz>
2018-05-09 21:50:22 +1200

    Missed return relation

--- app/src/Registration.php
+++ app/src/Registration.php
@@ -7,11 +7,16 @@ namespace Twohill\Example;
 
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Security\Member;
 
 class Registration extends DataObject
 {
     private static $table_name = 'Registration';
     private static $db = [
         'NameTag' => 'Varchar(255)'
+    ];
+
+    private static $has_one = [
+        'Member' => Member::class
     ];
 }
